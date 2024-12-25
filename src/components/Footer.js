ca0257ccf241e753772467b6783a1da59017c169
R.Britvasan <britvasan12@gmail.com>
2024-12-25 22:03:35 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,10 @@
-hello
+import React from 'react'
+
+const Footer = () => {
+    const year = new Date();
+  return (
+    <div className='py-4 bg-secondary text-center text-white font-text-font'>&copy; Britvasan {year.getFullYear()}</div>
+  )
+}
+
+export default Footer
